ac56b1ba1dfbae00e660a938ef19e2c080b2365b
Carlos Gochez <cgotcha@gmail.com>
2016-01-22 08:43:18 -0600

    Aqui se agrego las clases de prueba de Calendar y Arraylist

---
+++ src/intro/TestCalendar.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package intro;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author Aula
+ */
+public class TestCalendar {
+    public static void main(String[] args) {
+        Calendar c;
+    }
+}
