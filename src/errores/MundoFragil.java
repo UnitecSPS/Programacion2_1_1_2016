fa4a574ce29c1275e7e978fa53494f5abaeeae5a
Carlos Gochez <cgotcha@gmail.com>
2016-02-23 07:01:25 -0600

    Introduccion al manejo de errores

---
+++ src/errores/MundoFragil.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package errores;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Aula
+ */
+public class MundoFragil {
+    public static void main(String[] args) {
+        System.out.println("Iniciando Main");
+        a();
+        System.out.println("Finalizando Main");
+    }
+
+    private static void a() {
+        System.out.println("Iniciando A");
+        
+        try{
+            System.out.println("Iniciando Try");
+            b();
+            System.out.println("Finalizando Try");
+        }
+        catch(Exception e){
+            System.out.println("Error: " + e);
+        }
+        
+        System.out.println("Finalizando A");
+    }
+
+    private static void b() {
+        System.out.println("Iniciando B");
+        //codigos fragiles--------------------
+        Scanner lea = new Scanner(System.in);
+        int arr[] = { 2,0,1,6 };
+        int pos = lea.nextInt();
+        double div = 10 / arr[pos];
+        System.out.println("Division 10/"+arr[pos]+": "+ div );
+        //-----------------------------------
+        System.out.println("Finalizando B");
+    }
+}
