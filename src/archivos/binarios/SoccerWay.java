99a14ded1019fe5fc32a5426c6302ae59f93c126
Carlos Gochez <cgotcha@gmail.com>
2016-03-10 08:13:01 -0600

    Avanzar en el Soccerway System

--- src/archivos/binarios/SoccerWay.java
+++ src/archivos/binarios/SoccerWay.java
@@ -21,10 +21,15 @@ public class SoccerWay {
     RandomAccessFile rPlayers, rCods;
     public static String ROOT = "soccerway";
     
-    public SoccerWay() throws IOException{
-        new File(ROOT).mkdir();
-        rPlayers = new RandomAccessFile(ROOT+"/players.fut","rw");
-        initCodes();
+    public SoccerWay(){
+        try{
+            new File(ROOT).mkdir();
+            rPlayers = new RandomAccessFile(ROOT+"/players.fut","rw");
+            initCodes();
+        }
+        catch(IOException e){
+            System.out.println("No deberia pasar");
+        }
     }
 
     private void initCodes()throws IOException {
@@ -65,13 +70,80 @@ public class SoccerWay {
         return false;
     }
 
-    private boolean jerseyAssigned(int nc) {
+    private boolean jerseyAssigned(int nc)throws IOException {
+        rPlayers.seek(0);
+        while(rPlayers.getFilePointer() < rPlayers.length()){
+            rPlayers.readInt();
+            rPlayers.readUTF();
+            rPlayers.readLong();
+            rPlayers.readUTF();
+            rPlayers.readUTF();
+            int camisa = rPlayers.readInt();
+            rPlayers.readDouble();
+            
+            if(rPlayers.readBoolean() && nc == camisa){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public void printAvailables()throws IOException{
+        rPlayers.seek(0);
+        while(rPlayers.getFilePointer() < rPlayers.length()){
+            int cod = rPlayers.readInt();
+            String n = rPlayers.readUTF();
+            Date fecha = new Date(rPlayers.readLong());
+            String pos = rPlayers.readUTF();
+            String pais = rPlayers.readUTF();
+            int camisa = rPlayers.readInt();
+            double sal = rPlayers.readDouble();
+            
+            if(rPlayers.readBoolean()){
+                System.out.println(cod+"-"+n+"-"+pos+"-"+pais+
+                        "- $"+sal+" camisa: #"+camisa+" Nacido en: "+
+                        fecha);
+            }
+        }
+    }
+    
+    public boolean search(int cod)throws IOException{
         /*
-        1- Mover el puntero a 0
-        2- Recorrer todos los jugadores, viendo si alguno
-            tiene ese numero de camisa Y esta disponible.
-        3- Si hay alguno asi retorno true, sino, retorno false
+        retorne true si un jugador con ese codigo existe
         */
-        return true;
+        rPlayers.seek(0);
+        while(rPlayers.getFilePointer() < rPlayers.length()){
+            if( cod == rPlayers.readInt() )
+                return true;
+            rPlayers.readUTF();
+            rPlayers.readLong();
+            rPlayers.readUTF();
+            rPlayers.readUTF();
+            rPlayers.skipBytes(13);
+        }
+        return false;
+    }
+    
+    public boolean updateBirthdayFor(int cod, String fecha) throws IOException{
+        if(search(cod)){
+            //armo la fecha
+            String data[] = fecha.split("/");
+            int dia = Integer.parseInt(data[0]);
+            int mes = Integer.parseInt(data[1]) - 1;
+            int year = Integer.parseInt(data[2]);
+            Calendar birthday = Calendar.getInstance();
+            birthday.set(year, mes, dia);
+            
+            //si la fecha fue antes
+            if(birthday.before(Calendar.getInstance())){
+                //mi puntero por el search esta justo antes del nombre
+                rPlayers.readUTF();
+                //ahora viena la fecha!
+                rPlayers.writeLong(birthday.getTimeInMillis());
+                //no me importa ya los demas datos
+                return true;
+            }
+        }
+        return false;
     }
 }
